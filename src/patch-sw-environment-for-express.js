6eca4adfdd0df23c7fb8db8e97fd4514c14472dc
jjak0b <jacopo.rimediotti@studio.unibo.it>
2021-03-15 13:56:45 +0100

    fix(express): using patches of http and express modules

--- src/patch-sw-environment-for-express.js
+++ src/patch-sw-environment-for-express.js
@@ -1,43 +1,5 @@
 // patch and mock the environment
 
-// XMLHttpRequest is used to figure out the environment features
-if (typeof global.XMLHttpRequest === 'undefined') {
-  global.XMLHttpRequest = require('./XMLHttpRequest-mock')
-}
-
-// high resolution timestamps
-/* global performance */
-process.hrtime = performance.now.bind(performance)
-
-process.stdout = {
-  write: function fakeWrite (str) {
-    console.log(str)
-  }
-}
-
-// http structures used inside Express
-var http = require('http')
-if (!http.IncomingMessage) {
-  http.IncomingMessage = {}
-}
-
-if (!http.ServerResponse) {
-  http.ServerResponseProto = {
-    _headers: {},
-    setHeader: function setHeader (name, value) {
-      console.log('set header %s to %s', name, value)
-      this._headers[name] = value
-    },
-    getHeader: function getHeader (name) {
-      return this._headers[name]
-    },
-    get: function get (name) {
-      return this._headers[name]
-    }
-  }
-  http.ServerResponse = Object.create({}, http.ServerResponseProto)
-}
-
 // setImmediate is missing in the ServiceWorker
 if (typeof setImmediate === 'undefined') {
   global.setImmediate = function setImmediate (cb, param) {
@@ -45,19 +7,9 @@ if (typeof setImmediate === 'undefined') {
   }
 }
 
-// missing file system sync calls
-const fs = require('fs')
-if (typeof fs.existsSync === 'undefined') {
-  // mocking text file system :)
-  const __files = {}
-  fs.existsSync = function existsSync (path) {
-    return typeof __files[path] !== 'undefined'
-  }
-  fs.writeFileSync = function writeFileSync (path, text) {
-    // assuming utf8
-    __files[path] = text
-  }
-  fs.readFileSync = function readFileSync (path) {
-    return __files[path]
-  }
+module.exports = {
+  // patch http env
+  http: require('./http'),
+  // patch express env
+  express: require('./express').express
 }
